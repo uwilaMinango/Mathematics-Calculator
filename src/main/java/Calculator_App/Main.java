f0be888b3ffcb8313719018603a46fe2cf6ffbc8
Minango Uwila <uwilaminango2604@yahoo.com>
2025-08-16 14:17:59 +0200

    First Commit

---
+++ src/main/java/Calculator_App/Main.java
@@ -0,0 +1,51 @@
+/*Date: 16/08/2025
+* Developers Name: Minango Uwila
+* Program Description: A simple mathematics calculator system*/
+
+package Calculator_App;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Welcome to Calculator");
+        Scanner scan= new Scanner(System.in);
+        Operations ops = new Operations();
+
+        System.out.println("Please enter the complete mathematics operation:\n");
+        String mathOperation = scan.nextLine();
+
+
+        char [] operation = mathOperation.toCharArray();
+        for(char a : operation){
+            if(a=='+'){
+                String [] numbers = mathOperation.split("\\+");
+                double num1 = Double.parseDouble(numbers[0]);
+                double num2 = Double.parseDouble(numbers[1]);
+                System.out.println(ops.addition(num1, num2));
+                break;
+            }
+            else if(a=='-'){
+                String [] numbers = mathOperation.split("-");
+                double num1 = Double.parseDouble(numbers[0]);
+                double num2 = Double.parseDouble(numbers[1]);
+                System.out.println(ops.subtraction(num1, num2));
+                break;
+            }
+            else if(a=='*'){
+                String [] numbers = mathOperation.split("\\*");
+                double num1 = Double.parseDouble(numbers[0]);
+                double num2 = Double.parseDouble(numbers[1]);
+                System.out.println(ops.multiplication(num1, num2));
+                break;
+            }
+            else if(a=='/'){
+                String [] numbers = mathOperation.split("/");
+                double num1 = Double.parseDouble(numbers[0]);
+                double num2 = Double.parseDouble(numbers[1]);
+                System.out.println(ops.division(num1, num2));
+                break;
+            }
+
+        }
+    }
+}
---
+++ src/main/java/Calculator_App/Operations.java
@@ -0,0 +1,20 @@
+package Calculator_App;
+
+public class Operations {
+    public double addition(double num1, double num2){
+        return num1+num2;
+    }
+
+    public double subtraction(double num1, double num2){
+        return num1-num2;
+    }
+
+    public double multiplication(double num1, double num2){
+        return num1*num2;
+    }
+
+    public double division(double num1, double num2){
+        return num1/num2;
+    }
+
+}
